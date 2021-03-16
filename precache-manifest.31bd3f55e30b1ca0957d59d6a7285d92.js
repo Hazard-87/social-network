4b540e6812912b54bed044ecc469e5bfcc60f3c4
Hazard <dpetryshkin@gmail.com>
2021-03-16 13:41:13 +0300

    Updates

---
+++ precache-manifest.31bd3f55e30b1ca0957d59d6a7285d92.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ffc8dafd856fae9ec7d5aef5b68190",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "62c19a85599686564349",
+    "url": "/social-network/static/css/2.630bd408.chunk.css"
+  },
+  {
+    "revision": "5c4e2cc9499479801cbb",
+    "url": "/social-network/static/css/4.431fd661.chunk.css"
+  },
+  {
+    "revision": "18aba4c585fc78d282e7",
+    "url": "/social-network/static/css/5.9d46afea.chunk.css"
+  },
+  {
+    "revision": "2744e5619b0563ba2319",
+    "url": "/social-network/static/css/7.6fe75479.chunk.css"
+  },
+  {
+    "revision": "7d28fb658cf15beb4e41",
+    "url": "/social-network/static/css/main.3398af60.chunk.css"
+  },
+  {
+    "revision": "62c19a85599686564349",
+    "url": "/social-network/static/js/2.e358015d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social-network/static/js/2.e358015d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a63901963d5fbd263b",
+    "url": "/social-network/static/js/3.c9b72bd6.chunk.js"
+  },
+  {
+    "revision": "5c4e2cc9499479801cbb",
+    "url": "/social-network/static/js/4.10449456.chunk.js"
+  },
+  {
+    "revision": "18aba4c585fc78d282e7",
+    "url": "/social-network/static/js/5.e39d3145.chunk.js"
+  },
+  {
+    "revision": "05c25689fd1fc390f03b",
+    "url": "/social-network/static/js/6.0d2ee012.chunk.js"
+  },
+  {
+    "revision": "2744e5619b0563ba2319",
+    "url": "/social-network/static/js/7.3086af6a.chunk.js"
+  },
+  {
+    "revision": "7d28fb658cf15beb4e41",
+    "url": "/social-network/static/js/main.ec7d0bd9.chunk.js"
+  },
+  {
+    "revision": "062a591be49cd8cb4dbd",
+    "url": "/social-network/static/js/runtime-main.7f8f0272.js"
+  },
+  {
+    "revision": "4b0cb4ba419a9a61d6efcae770d84bde",
+    "url": "/social-network/static/media/user.4b0cb4ba.png"
+  }
+]);
